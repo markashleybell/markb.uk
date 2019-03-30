c5a0d922576daf0d38d5c5f6dc8f4489eefaf2d3
Mark Bell <me@markb.co.uk>
2019-03-30 17:57:10 +0000

    Gulp task overhaul

--- gulpfile.js
+++ gulpfile.js
@@ -5,39 +5,42 @@ const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
-const rename = require('gulp-rename');
 const filter = require('gulp-filter');
+const plumber = require('gulp-plumber');
 
-const compileCss = () => {
-    const scssFilter = filter('**/*.scss', { restore: true });
-
-    return gulp.src(['./css/vendor/*.css', './css/*.scss'])
-               .pipe(sourcemaps.init())
-               .pipe(scssFilter)
-               .pipe(sass())
-               .on('error', console.log)
-               .pipe(scssFilter.restore)
-               .pipe(cleanCSS())
-               .pipe(concat('bundle.css'))
-               .pipe(sourcemaps.write('.'))
-               .pipe(gulp.dest('./public/css'));
-}
+const scssFilter = filter('**/*.scss', { restore: true });
 
 const tsProject = typescript.createProject('tsconfig.json');
 
-const compileJs = () => {
-    return gulp.src(['./js/*.ts'])
-               .pipe(sourcemaps.init())
-               .pipe(tsProject())
-               .on('error', console.log)
-               .pipe(uglify())
-               .pipe(concat('bundle.js'))
-               .pipe(sourcemaps.write('.'))
-               .pipe(gulp.dest('./public/js'));
-}
+const compileCss = () => 
+    gulp.src(['./css/vendor/*.css', './css/*.scss'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(scssFilter)
+        .pipe(sass())
+        .pipe(scssFilter.restore)
+        .pipe(cleanCSS())
+        .pipe(concat('bundle.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/css'));
+
+const compileJs = () => 
+    gulp.src(['./js/*.ts'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(uglify())
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/js'));
 
 gulp.task('compile-css', compileCss);
 gulp.task('compile-js', compileJs);
 
-gulp.task('watch-css', () => gulp.watch('./css/*.scss', gulp.series(['compile-css'])));
-gulp.task('watch-js', () => gulp.watch('./js/*.ts', gulp.series(['compile-js'])));
+gulp.task('build', gulp.series('compile-css', 'compile-js'));
+
+gulp.task('watch', done => {
+    gulp.watch('./css/*.scss', gulp.series('compile-css'));
+    gulp.watch('./js/*.ts', gulp.series('compile-js'));
+    done();
+});
