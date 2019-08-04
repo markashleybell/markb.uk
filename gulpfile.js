cdf77a2841c0666fa6e6625f3dc664f204721067
Mark Bell <me@markb.co.uk>
2019-08-04 20:50:19 +0100

    Let's try *this* gulp syntax, maybe it'll work for more than a month or so this time
    I've moved the reset into the main SCSS file, because there was suddenly some weird error with the stream filter and frankly I'm completely done with wasting hours on crappy tools just so I can compile a CSS file.
    
    I've also changed to use the recommended gulp syntax because who knows, maybe the next time I open this project the gulp tasks will actually work, instead of mysteriously breaking every. single. time.

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,36 @@
-const gulp = require('gulp');
+const { dest, series, src, task, watch } = require('gulp');
 const sass = require('gulp-sass');
 const typescript = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
-const filter = require('gulp-filter');
-const plumber = require('gulp-plumber');
-
-const scssFilter = filter('**/*.scss', { restore: true });
 
 const tsProject = typescript.createProject('tsconfig.json');
 
-const compileCss = () => 
-    gulp.src(['./css/vendor/*.css', './css/*.scss'])
-        .pipe(plumber())
+function compileCss() {
+    return src(['./css/*.scss'])
         .pipe(sourcemaps.init())
-        .pipe(scssFilter)
         .pipe(sass())
-        .pipe(scssFilter.restore)
         .pipe(cleanCSS())
         .pipe(concat('bundle.css'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(dest('./public/css'));
+}
 
-const compileJs = () => 
-    gulp.src(['./js/*.ts'])
-        .pipe(plumber())
+function compileJs() {
+    return src(['./js/*.ts'])
         .pipe(sourcemaps.init())
         .pipe(tsProject())
         .pipe(uglify())
         .pipe(concat('bundle.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/js'));
+        .pipe(dest('./public/js'));
+}
 
-gulp.task('compile-css', compileCss);
-gulp.task('compile-js', compileJs);
+exports.build = series(compileCss, compileJs)
 
-gulp.task('build', gulp.series('compile-css', 'compile-js'));
-
-gulp.task('watch', done => {
-    gulp.watch('./css/*.scss', gulp.series('compile-css'));
-    gulp.watch('./js/*.ts', gulp.series('compile-js'));
-    done();
-});
+exports.watch = function() {
+    watch('./css/*.scss', compileCss);
+    watch('./js/*.ts', compileJs);
+}
