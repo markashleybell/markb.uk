8ca9ce77ed8f968e36586f4f424b864adf3978bf
Mark Bell <me@markb.co.uk>
2020-07-17 17:42:55 +0100

    Set up CI with Azure Pipelines

---
+++ config.ps1
@@ -0,0 +1,10 @@
+﻿$config = Get-Content -Path ".\config.cfg"
+
+$content = $config `
+    -ireplace "site_url: .*", "site_url: $env:SiteUrl" `
+    -ireplace "cdn1: .*", "cdn1: $env:CDN1Url" `
+    -ireplace "cdn2: .*", "cdn2: $env:CDN2Url" `
+    -ireplace "analytics_id: ANALYTICS_ID", "analytics_id: $env:AnalyticsID" `
+    -ireplace "disqus_id: DISQUS_ID", "disqus_id: $env:DisqusID"
+
+$content | Set-Content -Path ".\config.cfg"
