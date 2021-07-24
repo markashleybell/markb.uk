f998ccae0bc3ad1b5de488b070dfb35e0a1cc078
Mark Bell <me@markb.co.uk>
2021-07-24 17:29:23 +0100

    Config changes
    - Update file name for f-words 2.0
    - CDN URLs are no longer referenced (just serve assets relative to site)
    - Remove unused analytics/Disqus settings

--- config.ps1
+++ config.ps1
@@ -1,10 +1,6 @@
 ﻿$config = Get-Content -Path ".\config.cfg"
 
 $content = $config `
-    -ireplace "site_url: .*", "site_url: $env:SiteUrl" `
-    -ireplace "cdn1: .*", "cdn1: $env:CDN1Url" `
-    -ireplace "cdn2: .*", "cdn2: $env:CDN2Url" `
-    -ireplace "analytics_id: ANALYTICS_ID", "analytics_id: $env:AnalyticsID" `
-    -ireplace "disqus_id: DISQUS_ID", "disqus_id: $env:DisqusID"
+    -ireplace "site_url: .*", "site_url: $env:SiteUrl"
 
 $content | Set-Content -Path ".\config.cfg"
