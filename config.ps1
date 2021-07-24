7369de5a18dfc0a66d2c2412012d9305a02058b2
Mark Bell <me@markb.co.uk>
2021-07-24 17:32:04 +0100

    Rename config script and fix f-words config file name

--- config.ps1
+++
@@ -1,6 +0,0 @@
-﻿$config = Get-Content -Path ".\config.cfg"
-
-$content = $config `
-    -ireplace "site_url: .*", "site_url: $env:SiteUrl"
-
-$content | Set-Content -Path ".\config.cfg"
---
+++ update-config.ps1
@@ -0,0 +1,6 @@
+﻿$config = Get-Content -Path ".\fwords.cfg"
+
+$content = $config `
+    -ireplace "site_url: .*", "site_url: $env:SiteUrl"
+
+$content | Set-Content -Path ".\fwords.cfg"
